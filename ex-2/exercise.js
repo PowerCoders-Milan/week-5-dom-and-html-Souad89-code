7ea3b57f069c3bdd53e2371da52ba79a058be2ca
Souad Allahham <souadallahham89@gmail.com>
2021-03-02 14:09:28 +0100

    I finished ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,11 @@
-function getAttributes() {
 
+//href, hreflang, rel, target, and type attributes of the specified link.
+
+function getAttributes() {
+    const feature = document.querySelector("#pwcdr");
+    console.log(`The value of the href attribute of the link is : ${feature.href} `);
+    console.log(`The value of the type attribute of the link is : ${feature.type} `);
+    console.log(`The value of the target attribute of the link is : ${feature.target} `);
+    console.log(`The value of the rel attribute of the link is : ${feature.rel} `);
+    console.log(`The value of the hreflang attribute of the link is : ${feature.hreflang} `);      
 }
