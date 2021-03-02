39f4a598ce780c7b89fb48e95e9a5ec018d4530a
Souad Allahham <souadallahham89@gmail.com>
2021-03-02 14:38:58 +0100

    I finished ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,8 @@
 function insert_Row() {
-
+let myTable = document.getElementById("sampleTable");
+  let row = myTable.insertRow(0);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
 }
